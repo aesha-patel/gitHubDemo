c5cc0ccb42256c274aa1f019bd7fe2e6352e508f
Aesha Patel <aeshapatel@Aeshas-MacBook-Pro.local>
2018-07-25 12:38:13 -0400

    initial commit

---
+++ gitHubDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitHubDemo
+//
+//  Created by Aesha Patel on 2018-07-25.
+//  Copyright © 2018 Aesha Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
